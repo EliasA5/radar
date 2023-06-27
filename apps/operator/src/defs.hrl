22926e8cb2307f4f10d098024726d1d66a6ad633
Elias Assaf <elyas51000@gmail.com>
2023-06-27 20:05:39 +0300

    add some defs for communication

---
+++ apps/operator/src/defs.hrl
@@ -0,0 +1,12 @@
+
+-ifndef(DEFS_HRL).
+-define(DEFS_HRL, true).
+
+-define(MSPPC_ULTRASONIC,1).
+-define(MSPPC_LDR,2).
+-define(MSPPC_ACK,3).
+-define(PCMSP_TELEMETER,1).
+-define(PCMSP_FILE,2).
+-define(PCMSP_COMMAND,0).
+
+-endif.
