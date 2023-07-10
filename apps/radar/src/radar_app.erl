83ddd2933d4f4caf287be78b3b9d9ce45e7e19e7
Elias Assaf <elyas51000@gmail.com>
2023-07-10 23:52:40 +0300

    move supervisor into application

--- apps/radar/src/radar_app.erl
+++ apps/radar/src/radar_app.erl
@@ -6,14 +6,46 @@
 -module(radar_app).
 
 -behaviour(application).
+-behaviour(supervisor).
 
+-export([start_link/0]).
+
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    radar_sup:start_link().
+    radar_app:start_link().
 
 stop(_State) ->
     wx:destroy(),
     ok.
 
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%% sup_flags() = #{strategy => strategy(),         % optional
+%%                 intensity => non_neg_integer(), % optional
+%%                 period => pos_integer()}        % optional
+%% child_spec() = #{id => child_id(),       % mandatory
+%%                  start => mfargs(),      % mandatory
+%%                  restart => restart(),   % optional
+%%                  shutdown => shutdown(), % optional
+%%                  type => worker(),       % optional
+%%                  modules => modules()}   % optional
+init([]) ->
+    SupFlags = #{strategy => one_for_all,
+                 intensity => 0,
+                 period => 1,
+                 auto_shutdown => any_significant
+                },
+    ChildSpecs = [#{id => radar,
+                    start => {radar, start_link, []},
+                    restart => transient,
+                    significant => true
+                   }],
+    {ok, {SupFlags, ChildSpecs}}.
+
 %% internal functions
+
